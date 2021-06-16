c75baa55fcc558ac47cf3358b4e44e573dc52a89
Cipher <13287275832m0@sina.com>
2021-06-16 20:41:45 +0800

    Upload the code about the service of the fmprc

---
+++ internal/minserver/api/v1/fmprc/fmprc.go
@@ -0,0 +1,62 @@
+package fmprc
+
+import (
+	"fmt"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/parse"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/request"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/respont"
+	"net/http"
+)
+
+func GetDetailPageUrl(url string, baseUrl string) {
+	baseUrl = "https://www.fmprc.gov.cn/web/wjb_673085/zfxxgk_674865/gknrlb/zcfg/"
+	pr := respont.PR{
+		Request: request.Request{
+			Url:    url,
+			Method: http.MethodGet,
+		},
+		Parse: parse.Parse{
+			BaseUrl:     baseUrl,
+			UrlSelector: "div[class='jgbox_r fr'] a[target='_blank']",
+		},
+	}
+	pr.GetPageUrl("href")
+
+	//req := request.Request{
+	//	Url:    url,
+	//	Method: http.MethodGet,
+	//}
+	//html, err := req.Visit()
+	//if err != nil {
+	//	return
+	//}
+	//p := parse.Parse{
+	//	Html:        string(html),
+	//	UrlSelector: "div[class='jgbox_r fr'] a[target='_blank']",
+	//}
+	//hrefL := p.GetAllUrlByParseHtml("href")
+	//for _, href := range hrefL{
+	//	if strings.Contains(href, "http") || strings.Contains(href, "https"){
+	//		fmt.Println(href)
+	//	}else {
+	//		fmt.Println(baseUrl + href)
+	//	}
+	//}
+}
+
+func GetHtmlInfo(url string) (infoMap map[string]string) {
+	infoMap = make(map[string]string)
+	pr := respont.PR{
+		Request: request.Request{
+			Url:    url,
+			Method: http.MethodGet,
+		},
+		Parse: parse.Parse{
+			TitleSelector: "#News_Body_Title",
+			TextSelector:  "#News_Body_Txt_A>p",
+		},
+	}
+	infoMap = pr.GetHtmlInfo()
+	fmt.Println(infoMap)
+	return
+}
---
+++ internal/minserver/api/v1/fmprc/fmprc_test.go
@@ -0,0 +1,17 @@
+package fmprc
+
+import (
+	"testing"
+)
+
+func TestXpath(t *testing.T) {
+	url := "https://www.fmprc.gov.cn/web/wjb_673085/zfxxgk_674865/gknrlb/zcfg/"
+	GetDetailPageUrl(url, "")
+	infoUrl := "https://www.fmprc.gov.cn/web/wjb_673085/zfxxgk_674865/gknrlb/zcfg/./gfxwj/t1723270.shtml"
+	//infoUrl = "https://www.fmprc.gov.cn/web/wjb_673085/zfxxgk_674865/gknrlb/zcfg/./flfg/t70823.shtml"
+	//infoUrl = "http://www.caac.gov.cn/XXGK/XXGK/TZTG/202007/t20200721_203688.html"
+	GetHtmlInfo(infoUrl)
+
+}
+
+
