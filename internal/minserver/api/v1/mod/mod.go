5f2730642681ab18f029da9fef041e8aa06db852
Cipher <13287275832m0@sina.com>
2021-06-16 20:42:13 +0800

    Upload the code about the service of the mod

---
+++ internal/minserver/api/v1/mod/mod.go
@@ -0,0 +1,49 @@
+package mod
+
+import (
+	"fmt"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/parse"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/request"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/respont"
+	"net/http"
+)
+
+func GetFirstUrl(url string) {
+	fmt.Println(url) // frist url
+	for i := 2; i < 3; i++ {
+		url := fmt.Sprintf("%s_%v.htm", url[:len(url)-len(".htm")], i)
+		fmt.Println(url) // add other url
+	}
+}
+
+func GetDetailPageUrl(url string) {
+	baseurl := "http://www.mod.gov.cn/regulatory/"
+	pr := respont.PR{
+		Request: request.Request{
+			Url:    url,
+			Method: http.MethodGet,
+		},
+		Parse: parse.Parse{
+			BaseUrl:     baseurl,
+			UrlSelector: "#main-news-list>li>a",
+		},
+	}
+	pr.GetPageUrl("href")
+}
+
+func GetHtmlInfo(url string) (infoMap map[string]string){
+	infoMap = make(map[string]string)
+	pr := respont.PR{
+		Request: request.Request{
+			Url : url,
+			Method: http.MethodGet,
+		},
+		Parse:   parse.Parse{
+			TitleSelector: ".article-header>h1, .article-header>h3",
+			TextSelector: "#article-content>p",
+		},
+	}
+	infoMap = pr.GetHtmlInfo()
+	fmt.Println(infoMap)
+	return
+}
