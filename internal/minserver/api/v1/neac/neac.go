f87b199638af4d015a84b6b1bcba42d284742210
Cipher <13287275832m0@sina.com>
2021-06-17 15:52:14 +0800

    Upload the code about the service of the neac

---
+++ internal/minserver/api/v1/neac/neac.go
@@ -0,0 +1,63 @@
+package neac
+
+import (
+	"fmt"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/parse"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/request"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/respont"
+	"net/http"
+)
+
+func GetFirstUrl(url string) {
+	fmt.Println(url) // frist url
+	pr := respont.PR{
+		Request: request.Request{
+			Url:    url,
+			Method: http.MethodGet,
+		},
+		Parse: parse.Parse{
+			PageNumSelector: ".w1 script",
+		},
+	}
+	num := pr.GetPageNum(",\\d+,")
+	if num == 0 {
+		num = 100
+	}
+	for i := 2; i <= num; i++ {
+		url := fmt.Sprintf("%s_%v.shtml", url[:len(url)-len(".shtml")], i)
+		fmt.Println(url) // add other url
+	}
+}
+
+func GetDetailPageUrl(url string) {
+	pr := respont.PR{
+		Request: request.Request{
+			Url:    url,
+			Method: http.MethodGet,
+		},
+		Parse:   parse.Parse{
+			BaseUrl: url,
+			UrlSelector: ".w1 span>a",
+		},
+	}
+	pr.GetPageUrl("href")
+}
+
+
+func GetHtmlInfo(url string) (infoMap map[string]string){
+	infoMap = make(map[string]string)
+	pr := respont.PR{
+		Request: request.Request{
+			Url : url,
+			Method: http.MethodGet,
+		},
+		Parse:   parse.Parse{
+			DomainName: "https://www.neac.gov.cn",
+			TextSelector: "div[class='wzy p3']>p",
+			TitleSelector: ".p1",
+		},
+	}
+	infoMap = pr.GetHtmlInfo()
+	fmt.Println(infoMap)
+	return
+}
---
+++ internal/minserver/api/v1/neac/neac_test.go
@@ -0,0 +1,15 @@
+package neac
+
+import "testing"
+
+func TestXpath(t *testing.T) {
+
+	url := "https://www.neac.gov.cn/seac/xxgk/zcjd/index.shtml"
+	GetFirstUrl(url)
+	GetDetailPageUrl(url)
+	infoUrl := "https://www.neac.gov.cn/seac/xxgk/200703/1073907.shtml"
+	infoUrl = "https://www.neac.gov.cn/seac/xxgk/201704/1073893.shtml"
+	infoUrl = "https://www.neac.gov.cn/seac/xxgk/201108/1073902.shtml"
+	GetHtmlInfo(infoUrl)
+}
+
