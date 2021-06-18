b79a2fdffb564b229a45831343c83c39cda49656
Cipher <13287275832m0@sina.com>
2021-06-18 12:51:50 +0800

    Upload the code about the service of the mps

---
+++ internal/minserver/api/v1/mps/mps_test.go
@@ -0,0 +1,15 @@
+package mps
+
+import "testing"
+
+func TestXpath(t *testing.T) {
+
+	url := "https://www.mps.gov.cn/n6557558/index.html"
+	GetFirstUrl(url)
+	url = "https://www.mps.gov.cn/n6557558/index_7574611_14.html"
+	GetDetailPageUrl(url)
+	infoUrl := "https://www.mps.gov.cn/n6557558/c7789058/content.html"
+	infoUrl = "https://www.mps.gov.cn/n6557558/c3618764/content.html"
+	infoUrl = "https://www.mps.gov.cn/n6557558/c6836853/content.html"
+	GetHtmlInfo(infoUrl)
+}
