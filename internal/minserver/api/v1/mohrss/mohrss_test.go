ba14c23f2a1fa92cf0888fdaeedf012c8eb35b36
Cipher <13287275832m0@sina.com>
2021-06-20 16:37:17 +0800

    Upload the code about the service of the mohrss

--- internal/minserver/api/v1/mohrss/mohrss_test.go
+++ internal/minserver/api/v1/mohrss/mohrss_test.go
@@ -3,12 +3,12 @@ package mohrss
 import "testing"
 
 func TestXpath(t *testing.T) {
-	//url := "http://www.mohrss.gov.cn/govsearch/xxgkjs_new2020.jsp?orderby=date&default=isall&page=45"
-	//GetPageUrlList(url)
-	//GetDetailPageUrl(url)
+	url := "http://www.mohrss.gov.cn/govsearch/xxgkjs_new2020.jsp?orderby=date&default=isall&page=45"
+	GetPageUrlList(url)
+	GetDetailPageUrl(url)
 	infoUrl := "http://www.mohrss.gov.cn//xxgk2020/fdzdgknr/zcfg/bmgz/202011/t20201103_394781.html?keywords="
-	//infoUrl = "http://www.mohrss.gov.cn//xxgk2020/fdzdgknr/zcfg/gfxwj/rcrs/201407/t20140717_136235.html?keywords="
-	//infoUrl = "http://www.mohrss.gov.cn//xxgk2020/fdzdgknr/zcfg/gfxwj/rcrs/202105/t20210525_415135.html?keywords="
+	infoUrl = "http://www.mohrss.gov.cn//xxgk2020/fdzdgknr/zcfg/gfxwj/rcrs/201407/t20140717_136235.html?keywords="
+	infoUrl = "http://www.mohrss.gov.cn//xxgk2020/fdzdgknr/zcfg/gfxwj/rcrs/202105/t20210525_415135.html?keywords="
 	GetHtmlInfo(infoUrl)
 
 }
