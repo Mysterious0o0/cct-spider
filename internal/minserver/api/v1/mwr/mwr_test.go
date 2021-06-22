340c6cc22f2aca119e59f2ea943d3b3b6761a40d
Cipher <13287275832m0@sina.com>
2021-06-22 09:56:20 +0800

    Upload the code about the service of the mwr

--- internal/minserver/api/v1/mwr/mwr_test.go
+++ internal/minserver/api/v1/mwr/mwr_test.go
@@ -1,1 +1,17 @@
 package mwr
+
+import (
+	"testing"
+)
+
+func TestXpath(t *testing.T) {
+
+	//url := "http://www.mwr.gov.cn/zw/zcjd/"
+	//GetPageUrlList(url)
+	//GetDetailPageUrl(url)
+	infoUrl := "http://www.mwr.gov.cn/hd/zxft/zxzb/fbh20200821/"
+	infoUrl = "http://www.mwr.gov.cn/zw/zcjd/202009/t20200930_1449711.html"
+	infoUrl = "http://www.mwr.gov.cn/zw/zcjd/202004/t20200408_1399443.html"
+	GetHtmlInfo(infoUrl)
+
+}
