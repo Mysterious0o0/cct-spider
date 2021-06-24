f012c56548a6ba37b04e6e98a56b018340458bd4
Cipher <13287275832m0@sina.com>
2021-06-24 15:42:19 +0800

    Complete the main function writing

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"github.com/xiaogogonuo/cct-spider/internal/minserver/api/v1/miit"
+	"github.com/xiaogogonuo/cct-spider/internal/minserver/store"
 	"github.com/xiaogogonuo/cct-spider/pkg/config"
+	"sync"
 )
 
 func govConfig() *viper.Viper {
@@ -33,7 +36,45 @@ func minConfig() *viper.Viper {
 }
 
 func main() {
+	wg := &sync.WaitGroup{}
+	urlChannel := make(chan *store.UrlChan)
+	infoChannel := make(chan *store.InfoChan)
+	errChannel := make(chan *store.InfoChan)
+	infoMap := make(chan map[string]string)
 	minV := minConfig()
-	miitUrl := minV.GetString("工业和信息化部")
-	miit.GetFirstUrl(miitUrl)
+	wg.Add(1)
+	//go sarm.GetPageUrlList(minV.GetString("国家市场监督管理总局"), urlChannel, wg)
+	go miit.GetPageUrlList(minV.GetString("工业和信息化部"), urlChannel, wg)
+	//go mee.GetFirstUrl(minV.GetString("生态环境部"), urlChannel, wg)
+	//go cbirc.GetPageUrlList(minV.GetString("银保监会928"), infoChannel, wg)
+	//go cbirc.GetPageUrlList(minV.GetString("银保监会927"), infoChannel, wg)
+
+
+	go func() {
+		for v := range urlChannel{
+			wg.Add(1)
+			go v.GetUrlFunc(urlChannel, infoChannel, wg)
+
+		}
+	}()
+	go func() {
+		for v := range infoChannel{
+			wg.Add(1)
+			go v.GetInfoFunc(errChannel, infoMap, wg)
+		}
+	}()
+	go func() {
+		wg.Wait()
+		close(urlChannel)
+		close(infoChannel)
+		close(infoMap)
+	}()
+
+	for info := range infoMap{
+		for _, k := range info{
+			if k != "" {
+				fmt.Println(info)
+			}
+		}
+	}
 }
