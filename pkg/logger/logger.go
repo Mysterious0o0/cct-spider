7c68d8157be10e25ae995832fe7c1a2878264d28
xiaogougounuo <xiaogougounuo@163.com>
2021-07-02 11:41:30 +0800

    modify log plugin

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -11,8 +11,8 @@ const (
 	logConsole = true                 // 是否打印日志到终端
 	JsonFormat = true                 // 日志保存格式是否采用json类型
 	Filename   = "log/cct-spider.log" // 日志保存路径
-	MaxSize    = 10                   // 每个日志文件保存10M，默认100M
-	MaxBackups = 30                   // 保留30个备份，默认不限
+	MaxSize    = 100                  // 每个日志文件保存几M，默认100M
+	MaxBackups = 30                   // 保留多少个备份，默认不限
 	MaxAge     = 0                    // 保留多少天，默认不限
 	Compress   = false                // 是否压缩，默认不压缩
 )
@@ -65,8 +65,8 @@ func init() {
 
 func writerSyncer() zapcore.WriteSyncer {
 	hook := &lumberjack.Logger{
-		Filename:   Filename,
-		Compress:   Compress,
+		Filename: Filename,
+		Compress: Compress,
 	}
 	if MaxBackups > 0 {
 		hook.MaxBackups = MaxBackups
