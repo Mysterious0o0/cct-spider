608d6c29e18f29e4ceb22bec679e778756c60ef4
Cipher <13287275832m0@sina.com>
2021-07-16 14:34:32 +0800

    Add region field

--- internal/minserver/store/mysql.go
+++ internal/minserver/store/mysql.go
@@ -4,37 +4,63 @@ import (
 	"fmt"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/callback"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/filter"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/findregion"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/subString"
 	"github.com/xiaogogonuo/cct-spider/pkg/db/mysql"
 	"github.com/xiaogogonuo/cct-spider/pkg/encrypt/md5"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
 var (
+	regionPat                       string
+	regionMap                       map[string]string
 	s                               = &callback.SqlValues{}
 	t                               = time.Now().Format("20060102")
 	preamble, epilogue, oneQuoteSql = _getSQL(s)
 )
 
+func init() {
+	var regionReg []string
+	regionMap = make(map[string]string)
+	sqlCode := "SELECT REGION_NAME, REGION_CODE FROM T_DMAA_REGIOIN_CODE"
+	for _, region := range mysql.Query(sqlCode) {
+		reg := regexp.MustCompile("省|市|自治区|自治州|特别行政区|区|盟")
+		r := reg.ReplaceAllString(region[0], ")")
+		regionReg = append(regionReg, r)
+		k := strings.Replace(r, ")", "", -1)
+		if _, ok := regionMap[k]; !ok {
+			regionMap[k] = region[1]
+		}
+	}
+	regionPat = "(" + strings.Join(regionReg, "|(")
+}
+
 func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 	var (
+		region       string
+		regionCode   string
 		quotes       []string
 		insertValues []interface{}
 		beginLen     = len(preamble) + len(epilogue)
 	)
 	for mes := range message {
-		if len(mes.Title) == 0 && len(mes.Content) == 0 {
+		if len(mes.Title) == 0 && len(mes.Summary) == 0 {
 			continue
 		}
-		if mes.Date > t || mes.Date == "" {
+		if mes.Date == "" || mes.Date > t {
 			mes.Date = "10000101"
 		}
-		if len(mes.Content) > 65535 {
-			n, _ := subString.RuneIndex([]byte(mes.Content), 65535/3)
-			mes.Content = mes.Content[:n]
+		if len(mes.Summary) > 65535 {
+			n, _ := subString.RuneIndex([]byte(mes.Summary), 65535/3)
+			mes.Summary = mes.Summary[:n]
+		}
+		if r := findregion.FindRegion(regionPat, mes.Summary); r != "" {
+			region = r
+			regionCode = regionMap[r]
 		}
 		sqlValues := &callback.SqlValues{
 			NEWS_GUID:        md5.MD5(mes.Date + mes.Title),
@@ -42,17 +68,22 @@ func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 			NEWS_TS:          mes.Date,
 			NEWS_URL:         mes.Url,
 			NEWS_SOURCE:      mes.Source,
-			NEWS_SUMMARY:     mes.Content,
+			NEWS_SOURCE_CODE: mes.SourceCode,
+			NEWS_SUMMARY:     mes.Summary,
 			POLICY_TYPE:      "10",
 			POLICY_TYPE_NAME: "国家政策",
-			IS_CONTROL:       "否",
-			IS_INVEST:        "否",
-			IS_DEPOSIT:       "否",
-			IS_FUND:          "否",
-			IS_STOCK:         "否",
-			IS_FINANCE:       "否",
-			IS_INDUSTRY:      "否",
-			IS_CAPITAL:       "否",
+			REGION_NAME:      region,
+			REGION_CODE:      regionCode,
+			IS_CONTROL:       "N",
+			IS_INVEST:        "N",
+			IS_DEPOSIT:       "N",
+			IS_FUND:          "N",
+			IS_STOCK:         "N",
+			IS_FINANCE:       "N",
+			IS_INDUSTRY:      "N",
+			IS_CAPITAL:       "N",
+			NEWS_GYS_CODE:    "90",
+			NEWS_GYS_NAME:    "爬虫",
 		}
 		//logger.Info("Success", logger.Field("url", mes.Url))
 		f.WriteMap(md5.MD5(mes.Url))
