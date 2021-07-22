1044ec67470afc42361fa8cfb7c485abc5f73b7c
Cipher <13287275832m0@sina.com>
2021-07-22 18:52:09 +0800

    Adapt to new public components

--- internal/minserver/store/mysql.go
+++ internal/minserver/store/mysql.go
@@ -4,40 +4,19 @@ import (
 	"fmt"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/callback"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/filter"
-	"github.com/xiaogogonuo/cct-spider/internal/pkg/findregion"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/findmap"
+	"github.com/xiaogogonuo/cct-spider/internal/pkg/splitsql"
 	"github.com/xiaogogonuo/cct-spider/internal/pkg/subString"
 	"github.com/xiaogogonuo/cct-spider/pkg/db/mysql"
 	"github.com/xiaogogonuo/cct-spider/pkg/encrypt/md5"
-	"reflect"
-	"regexp"
-	"strconv"
 	"strings"
 	"time"
 )
 
 var (
-	regionPat                       string
-	regionMap                       map[string][]string
-	s                               = &callback.SqlValues{}
 	t                               = time.Now().Format("20060102")
-	preamble, epilogue, oneQuoteSql = _getSQL(s)
+	preamble, epilogue, oneQuoteSql = splitsql.GetInsertBaseSQLCode(&callback.SqlValues{}, "t_dmbe_policy_news_info")
 )
-
-func init() {
-	var regionReg []string
-	regionMap = make(map[string][]string)
-	sqlCode := "SELECT REGION_NAME, REGION_CODE FROM T_DMAA_REGIOIN_CODE"
-	for _, region := range mysql.Query(sqlCode) {
-		reg := regexp.MustCompile("省|市|自治区|自治州|特别行政区|区|盟")
-		r := reg.ReplaceAllString(region[0], ")")
-		regionReg = append(regionReg, r)
-		k := strings.Replace(r, ")", "", -1)
-		if _, ok := regionMap[k]; !ok {
-			regionMap[k] = region
-		}
-	}
-	regionPat = "(" + strings.Join(regionReg, "|(")
-}
 
 func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 	var (
@@ -58,7 +37,8 @@ func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 			n, _ := subString.RuneIndex([]byte(mes.Summary), 65535/3)
 			mes.Summary = mes.Summary[:n]
 		}
-		if r := findregion.FindRegion(regionPat, mes.Summary); r != "" {
+		regionPat, regionMap := findmap.RegionRuntime()
+		if r := findmap.FindOne(regionPat, mes.Summary); r != "" {
 			region = regionMap[r][0]
 			regionCode = regionMap[r][1]
 		}
@@ -86,7 +66,7 @@ func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 			NEWS_GYS_NAME:    "爬虫",
 		}
 		f.WriteMap(md5.MD5(mes.Url))
-		v, l := _getQuotesAndValues(sqlValues)
+		v, l := splitsql.GetQuotesAndValues(sqlValues)
 
 		if beginLen+l+len(oneQuoteSql) < 500000 {
 			insertValues = append(insertValues, v...)
@@ -102,67 +82,10 @@ func InsertIntoSQL(f *filter.Filter, message <-chan *callback.Message) {
 			beginLen = len(preamble) + len(epilogue) + len(oneQuoteSql) + l
 		}
 	}
+	if len(insertValues) == 0{
+		return
+	}
 	SQl := fmt.Sprintf("%s%s %s", preamble, strings.Join(quotes, ", "), epilogue)
 	mysql.Transaction(SQl, insertValues...)
 	f.SaveUrlKey()
-}
-
-func _getQuotesAndValues(v interface{}) (insertValues []interface{}, strLen int) {
-	insertValues = make([]interface{}, 0)
-
-	elem := reflect.ValueOf(v)
-
-	if elem.Kind() == reflect.Ptr {
-		elem = elem.Elem()
-	}
-	for i := 0; i < elem.NumField(); i++ {
-		curField := elem.Field(i)
-		val, valLen := _parseField(curField)
-		strLen += valLen
-		insertValues = append(insertValues, val)
-	}
-	return
-}
-
-func _parseField(v reflect.Value) (fieldValue interface{}, valLen int) {
-
-	switch v.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		fieldValue = v.Int()
-		valLen = len(strconv.FormatInt(v.Int(), 10))
-
-	case reflect.String:
-		fieldValue = v.String()
-		valLen = v.Len()
-	}
-	return
-
-}
-
-func _getSQL(v interface{}) (preambleSql string, epilogueSql string, oneQuoteSql string) {
-
-	elem := reflect.ValueOf(v)
-	if elem.Kind() == reflect.Ptr {
-		elem = elem.Elem()
-	}
-
-	elemType := elem.Type()
-	numFields := elem.NumField()
-
-	quotes := strings.Repeat("?,", numFields)
-	quotes = quotes[0 : len(quotes)-1]
-
-	insertFields := make([]string, 0, numFields)
-	epilogues := make([]string, 0, numFields)
-
-	for i := 0; i < numFields; i++ {
-		field := elemType.Field(i).Name
-		insertFields = append(insertFields, field)
-		epilogues = append(epilogues, fmt.Sprintf("%s = VALUES(%s)", field, field))
-
-	}
-	oneQuoteSql = fmt.Sprintf("(%s)", quotes)
-	preambleSql = fmt.Sprintf("INSERT INTO t_dmbe_policy_news_info (%s) VALUES ", strings.Join(insertFields, ", "))
-	epilogueSql = fmt.Sprintf("ON DUPLICATE KEY UPDATE %s", strings.Join(epilogues, ", "))
-	return
 }
